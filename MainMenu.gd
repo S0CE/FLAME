b831f2cb5c18cecfd2269b2954a3d64e245d8a40
Quentin Stoll <quentin.stoll@epitech.eu>
2024-05-19 00:19:28 +0200

    add: game music

--- MainMenu.gd
+++ MainMenu.gd
@@ -8,6 +8,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if $AudioStreamPlayer.playing == false:
+		$AudioStreamPlayer.play()
 	pass
 
 
