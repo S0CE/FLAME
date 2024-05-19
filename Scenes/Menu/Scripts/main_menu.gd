4d56abe55ca73997836b60abb1838c59f4c3d7c9
LaurentZHANG <laurent.zhang@epitech.eu>
2024-05-19 03:55:31 +0200

    fix: put main menu music to autoplay and remove unused functions

--- Scenes/Menu/Scripts/main_menu.gd
+++ Scenes/Menu/Scripts/main_menu.gd
@@ -6,15 +6,6 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if $AudioStreamPlayer.playing == false:
-		$AudioStreamPlayer.play()
-	pass
-
-	
-
-
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Level/level-1.tscn")
 
